45fe0a4b0b1ff28aabaa2b79d25ed7e70c765005
Taliyah Wilson <taliyahwilson27@gmail.com>
2020-08-06 02:55:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+katz_deli = []
+def line(array)
+  line_position = []
+  if array.length == 0 
+    puts "The line is currently empty."
+  else
+    counter = 1
+    array.each do |customer|
+      line_position << "#{counter}. #{customer}"
+      counter += 1
+    end
+    puts "The line is currently: #{line_position.join(" ")}"
+  end
+end
+
+def take_a_number(katz_deli, name)
+  
+end
+
+def now_serving()
+end

64fe6ad6065dc290a8c4a0bd65e70b6d6353e9b1
Taliyah Wilson <taliyahwilson27@gmail.com>
2020-08-06 03:15:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,7 @@ end
 
 def now_serving(line)
   if line.length == 0 
-    puts "Line is empty."
+    puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{line.first}."
   end
